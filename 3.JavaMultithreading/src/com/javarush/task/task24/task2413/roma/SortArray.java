9f47f81d21bb612e9abe091ac15e9058dd73ca80
roman <r.znamerovskiy@gmail.com>
2018-12-11 09:56:03 +0200

    My 13 commit. From task 2413 to task 3608(in Progress) This tasks is going one after another.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/roma/SortArray.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task24.task2413.roma;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortArray {
+//    public static void main(String[] args) {
+//        int[] arrInt = new int[20];
+//        for (int i = 0; i < arrInt.length; i++) {
+//            arrInt[i] = (int) (Math.random() * 1000);
+//        }
+//
+//        bubleSortReverse(arrInt);
+//        System.out.println(Arrays.toString(arrInt));
+//
+//        String[] carBrands = {"Opel", "BMW", "Mazda", "Nissan"};
+//        Arrays.sort(carBrands);
+//        System.out.println(Arrays.toString(carBrands));
+//        Arrays.sort(carBrands, Collections.reverseOrder());
+//        System.out.println(Arrays.toString(carBrands));
+//    }
+//
+//    private static void bubleSortReverse(int[] arrInt) {
+//        for (int i = 0; i < arrInt.length; i++) {
+//            for (int j = arrInt.length - 1; j > i; j--) {
+//                if (arrInt[j - 1] < arrInt[j]) {
+//                    int a = arrInt[j - 1];
+//                    arrInt[j - 1] = arrInt[j];
+//                    arrInt[j] = a;
+//                }
+//            }
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
